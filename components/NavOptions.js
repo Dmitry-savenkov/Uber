d93e1e68429ded0154fab029622e68c4f836f65f
Dmitry_savenkov <dmitry2002savenkov@mail.ru>
2021-11-13 18:13:09 +0300

    uber2.0 - [HomeScreen] building the homeScreen component

---
+++ components/NavOptions.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const NavOptions = () => {
+
+    const data = [
+        {
+            id: '123',
+            title: 'Get a ride',
+            image: 'https://links.papareact.com/3pn',
+            screen: 'MapScreen',
+        },
+        {
+            id: '456',
+            title: 'Order food',
+            image: 'https://links.papareact.com/28w',
+            screen: 'EatsScreen',
+        },
+    ];
+
+    return (
+        <View>
+            <Text>h</Text>
+        </View>
+    )
+}
+
+export default NavOptions
+
+const styles = StyleSheet.create({})
