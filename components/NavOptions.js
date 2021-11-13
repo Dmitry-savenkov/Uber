185f71d345a0e54f3e23e4936b936a14e2c02a15
Dmitry_savenkov <dmitry2002savenkov@mail.ru>
2021-11-13 18:47:20 +0300

    uber2.0 [components] add NavOptions component with data and links

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
+import { Icon } from 'react-native-elements'
+import tw from 'tailwind-react-native-classnames'
 
 const NavOptions = () => {
 
@@ -19,9 +21,30 @@ const NavOptions = () => {
     ];
 
     return (
-        <View>
-            <Text>h</Text>
-        </View>
+        <FlatList
+          data={data}
+          horizontal
+          keyExtractor={( item ) => item.id }
+          renderItem={({ item }) => (
+            <TouchableOpacity style={[tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`]}>
+                <View>
+                <Image
+                    style={{
+                    width:120,
+                    height:120,
+                    resizeMode: 'contain',
+                }}
+                source={{
+                    uri: item.image
+                }}
+                />
+                <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+                <Icon style={tw`p-2 bg-black rounded-full w-10 mt-4`} name='arrowright' color='white' type='antdesign'/>
+                </View>
+            </TouchableOpacity>
+          )}
+        />
+
     )
 }
 
