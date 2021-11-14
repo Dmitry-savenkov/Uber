c4476eb773647bd78d77860253490486a8114cef
Dmitry_savenkov <dmitry2002savenkov@mail.ru>
2021-11-14 13:20:11 +0300

    uber2.0 - [mapScreen] add mapScreen component and update search by value in input

---
+++ components/Map.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import MapView, { Marker } from 'react-native-maps';
+import { useSelector } from 'react-redux';
+import tw from 'tailwind-react-native-classnames'
+import { selectOrigin } from '../slices/navSlices';
+
+const Map = () => {
+
+    const origin = useSelector(selectOrigin)
+
+    return (
+        <MapView
+        mapType='mutedStandard'
+        style={[tw`flex-1`]}
+    initialRegion={{
+      latitude: origin.location.lat,
+      longitude: origin.location.lng,
+      latitudeDelta: 0.005,
+      longitudeDelta: 0.005,
+    }}
+  />
+    )
+}
+
+export default Map;
+
+const styles = StyleSheet.create({})
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Image} from 'react-native';
 import tw from 'tailwind-react-native-classnames'
 import NavOptions from '../components/NavOptions';
+import Map from '../components/Map';
 
 export default function MapScreen() {
   return (
-      <SafeAreaView style={[tw`bg-white h-full`]}>
-        <Text>hi</Text>
-      </SafeAreaView>
+      <View>
+        <View style={[tw`h-1/2`]}>
+          <Map />
+        </View>
+        <View style={[tw`h-1/2`]}></View>
+      </View>
   );
 }
 
