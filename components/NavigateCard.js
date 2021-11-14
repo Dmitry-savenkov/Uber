0987cba5a83a326674ecca1071d54bba4d2c4704
Dmitry_savenkov <dmitry2002savenkov@mail.ru>
2021-11-14 17:16:08 +0300

    uber2.0 [components] update navigateCard

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,14 +1,62 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView} from 'react-native'
+import tw from 'tailwind-react-native-classnames'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { useDispatch } from 'react-redux';
+import { setOrigin, setDestination } from '../slices/navSlices'
+import { useNavigation } from '@react-navigation/core';
+const API_KEY = "AIzaSyDzpKfhmv_n_OyvEZYaCM6l-bDTRUKgp8E"
 
 const NavigateCard = () => {
+
+    const dispatch = useDispatch();
+    const navigation = useNavigation()
+
     return (
-        <View>
-            <Text>sodkfksdf;k</Text>
-        </View>
+        <SafeAreaView style={[tw`bg-white flex-1`]}>
+            <Text>123</Text>
+            <View style={[tw`border-t border-gray-200 flex-shrink`]}>
+            <GooglePlacesAutocomplete
+              placeholder='Where to?'
+              fetchDetails={true}
+              styles={toInputBoxStyles}
+              fetchDetails={true}
+              enablePoweredByContainer={false}
+              nearbyPlacesAPI='GooglePlacesSearch'
+              query={{
+                key: API_KEY,
+                language: 'en'
+              }}
+              onPress={(data, details = null) => {
+                dispatch(setDestination({
+                  location: details.geometry.location,
+                  description: data.description
+                }))
+                navigation.navigate('RideOptionsCard')
+              }}
+              minLength={2}
+              debounce={400}
+            />
+            </View>
+        </SafeAreaView>
     )
 }
 
 export default NavigateCard;
 
-const styles = StyleSheet.create({})
+const toInputBoxStyles = StyleSheet.create({
+    container: {
+      backgroundColor: 'white',
+      paddingTop: 20,
+      flex: 0,
+    },
+    textInput:{
+        backgroundColor:'#DDDDDF',
+        borderRadius:0,
+        fontSize:18,
+    },
+    textInputContainer:{
+        paddingHorizontal: 20,
+        paddingBottom: 20,
+    }
+})
